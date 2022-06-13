9175c1735274152807fc94d017524dd13b3eb99c
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-13 04:31:32 -0600

    Display the map using maboxlg is ready. Also I added a marker and a popup.

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,20 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { PlacesService } from '../../services';
+import { Map, Marker, Popup } from 'mapbox-gl';
 
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
-  styleUrls: ['./map-view.component.css']
+  styleUrls: ['./map-view.component.css'],
 })
-export class MapViewComponent implements OnInit {
+export class MapViewComponent implements AfterViewInit {
   userLocation: [number, number] | undefined;
+  @ViewChild('map') mapElement!: ElementRef;
 
-  constructor(private placesService: PlacesService) { }
-
-  ngOnInit(): void {
+  constructor(private placesService: PlacesService) {}
+  ngAfterViewInit(): void {
     this.userLocation = this.placesService.userLocation;
-    console.log(this.userLocation);
-    
-  }
+    if (!this.userLocation) return;
 
+    const map = new Map({
+      container: this.mapElement.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.userLocation,
+      zoom: 9,
+    });
+
+    const popup = new Popup().setHTML(`
+        <h6>I'm here!</h6>
+        <span>In this place in the world!</span>
+      `);
+    new Marker({ color: 'red' })
+      .setPopup(popup)
+      .setLngLat(this.userLocation)
+      .addTo(map);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import mapboxgl from 'mapbox-gl';
+ 
+mapboxgl.accessToken = environment.mapboxToken;
+
 
 if(!navigator.geolocation){
   alert('Geolocation is not supported by your browser');
