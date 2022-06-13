1a7f6f4fe68354c23661d17ce53ae9d770c9b957
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-13 03:33:04 -0600

    Initial setup.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+if(!navigator.geolocation){
+  alert('Geolocation is not supported by your browser');
+  throw new Error('Geolocation is not supported by your browser');
+}
+
 if (environment.production) {
   enableProdMode();
 }
